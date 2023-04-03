f4d03aa7889774e54fcd5ed0ffcc81b615246d31
Shiffana Nazeema A <63502422+shiffananazeema@users.noreply.github.com>
2023-04-03 15:58:57 +0530

    Update Program.py

--- Program.py
+++ Program.py
@@ -25,6 +25,10 @@ a, b = b, a
 	
 squares = [i*i for i in range(10) if i % 2 == 0]
 
+squares = [i*i for i in range(10) if i % 2 == 0]
+print(squares)
+[0, 4, 16, 36, 64]
+
 
 3. If-Else Ternary Operator
 	
@@ -173,24 +177,18 @@ while a < 50:
     a, b = b, a + b
 '''
 
+#11 - Second Last
 '''
-squares = [i*i for i in range(10) if i % 2 == 0]
-print(squares)
-[0, 4, 16, 36, 64]
+if __name__ == '__main__':
+    records = {}
+    
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        records[name] = score
+        
+    second_largest_value = sorted(set(records.values()))[1]
+    for i, j in sorted(records.items()):
+        if j==second_largest_value:
+            print(i)
 '''
-
-# -
-'''
-
-'''
-def transform(a = 2):
-    if a == 1:
-        return a +- 2
-    return a
-
-total = 1
-
-for x in [3,5,1]:
-    total = total + transform(x)
-
-print(total)
